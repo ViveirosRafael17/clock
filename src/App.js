fcf278ef0c63d5340c8ee6a8ae1d42e7d150f0fb
Rafael Viveiros <rafavbarbosa@gmail.com>
2022-02-18 16:25:56 -0300

    primeira versão de relogio digital funcional

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let hora = document.getElementById('hora');
+let minute = document.getElementById('minute');
+
+const degHora = () => {
+  let date = new Date();
+  let hour = date.getHours();
+  let deg = 30 * hour + 270;
+  return deg;
+}
+
+
+const degMinute = () => {
+  let date = new Date();
+  let min = date.getMinutes();
+  let deg = 6 * min + 270;
+  return deg;
+}
+
+setInterval(() => {
+  window.location = window.location.href
+}, 3000)
+
+hora.style.transform = `rotate(${degHora()}deg)`
+minute.style.transform = `rotate(${degMinute()}deg)`
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date ()}
+  }
+  render() {
+    return (
+      <div>
+        <p id="date" class="date">{this.state.date.toLocaleTimeString()}</p>
+      </div>
+    )
+  }
+  componentDidMount() {
+    const oneSecond = 1000;
+    setInterval(() => {
+      this.setState({date: new Date()})
+    }, oneSecond)
+  }
 }
 
 export default App;
